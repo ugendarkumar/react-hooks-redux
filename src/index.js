6b5cd980792c8256e2a8cea768166d7c3310c113
ugendarkumar <ugendar@yahoo.in>
2020-06-24 18:42:54 +0530

    code update

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import ProductsProvider from './context/products-context';
+
+import './index.css';
+import App from './App';
+import productReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  shop: productReducer
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+  {/* <ProductsProvider> */}
+<BrowserRouter>
+      <App />
+    </BrowserRouter>
+
+  {/* // </ProductsProvider> */}
+       </Provider>,
+
+  document.getElementById('root')
+);
